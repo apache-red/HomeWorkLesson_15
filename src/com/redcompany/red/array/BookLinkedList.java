b569641a0decd7338e1dd708a5e7539a74a875c5
apache-red <apache_red@mail.ru>
2018-11-26 12:10:21 +0300

    init project

---
+++ src/com/redcompany/red/array/BookLinkedList.java
@@ -0,0 +1,154 @@
+package com.redcompany.red.array;
+
+import java.util.Iterator;
+
+public class BookLinkedList implements Dequee {
+
+
+    @Override
+    public void addFirst(Object o) {
+
+    }
+
+    @Override
+    public void addLast(Object o) {
+
+    }
+
+    @Override
+    public boolean offerFirst(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean offerLast(Object o) {
+        return false;
+    }
+
+    @Override
+    public Object removeFirst() {
+        return null;
+    }
+
+    @Override
+    public Object removeLast() {
+        return null;
+    }
+
+    @Override
+    public Object pollFirst() {
+        return null;
+    }
+
+    @Override
+    public Object pollLast() {
+        return null;
+    }
+
+    @Override
+    public Object getFirst() {
+        return null;
+    }
+
+    @Override
+    public Object getLast() {
+        return null;
+    }
+
+    @Override
+    public Object peekFirst() {
+        return null;
+    }
+
+    @Override
+    public Object peekLast() {
+        return null;
+    }
+
+    @Override
+    public boolean removeFirstOccurrence(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean removeLastOccurrence(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean add(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean offer(Object o) {
+        return false;
+    }
+
+    @Override
+    public Object remove() {
+        return null;
+    }
+
+    @Override
+    public Object poll() {
+        return null;
+    }
+
+    @Override
+    public Object element() {
+        return null;
+    }
+
+    @Override
+    public Object peek() {
+        return null;
+    }
+
+    @Override
+    public void push(Object o) {
+
+    }
+
+    @Override
+    public Object pop() {
+        return null;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public Iterator descendingIterator() {
+        return null;
+    }
+
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+}
---
+++ src/com/redcompany/red/array/Dequee.java
@@ -0,0 +1,64 @@
+package com.redcompany.red.array;
+
+
+import java.util.Iterator;
+
+
+public interface Dequee<E> {
+
+    void addFirst(E e);
+
+    void addLast(E e);
+
+    boolean offerFirst(E e);
+
+    boolean offerLast(E e);
+
+    E removeFirst();
+
+    E removeLast();
+
+    E pollFirst();
+
+    E pollLast();
+
+    E getFirst();
+
+    E getLast();
+
+    E peekFirst();
+
+    E peekLast();
+
+    boolean removeFirstOccurrence(Object o);
+
+    boolean removeLastOccurrence(Object o);
+
+    boolean add(E e);
+
+    boolean offer(E e);
+
+    E remove();
+
+
+    E poll();
+
+    E element();
+
+    E peek();
+
+    void push(E e);
+
+    E pop();
+
+    boolean remove(Object o);
+
+    boolean contains(Object o);
+
+    public int size();
+
+    Iterator<E> iterator();
+
+    Iterator<E> descendingIterator();
+
+}
